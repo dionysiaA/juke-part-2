3b1364084d95c278021fa7f916551dec3bdbea69
Denise Agathocleous <denise_agathocleous@hotmail.com>
2017-03-28 17:41:03 -0400

    initial state of workshop

--- browser/react/containers/AppContainer.js
+++ browser/react/containers/AppContainer.js
@@ -103,6 +103,7 @@ export default class AppContainer extends Component {
   }
 
   render () {
+    { console.log(this.props.children) }
     return (
       <div id="main" className="container-fluid">
         <div className="col-xs-2">
@@ -110,17 +111,20 @@ export default class AppContainer extends Component {
         </div>
         <div className="col-xs-10">
         {
-          this.state.selectedAlbum.id ?
-          <Album
-            album={this.state.selectedAlbum}
-            currentSong={this.state.currentSong}
-            isPlaying={this.state.isPlaying}
-            toggleOne={this.toggleOne}
-          /> :
-          <Albums
-            albums={this.state.albums}
-            selectAlbum={this.selectAlbum}
-          />
+          this.props.children ?
+          React.cloneElement( this.props.children,
+            {
+              album: this.state.selectedAlbum,
+              currentSong: this.state.currentSong,
+              isPlaying: this.state.isPlaying,
+              toggleOne: this.toggleOne,
+
+              // albums
+              albums: this.state.albums,
+              selectedAlbum: this.selectAlbum
+            })
+           :
+         null
         }
         </div>
         <Player
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import { Router, Route, hashHistory } from 'react-router';
 
 ReactDOM.render(
-  <AppContainer />,
+  <Router history={hashHistory}>
+    <Route path="/" component={AppContainer} >
+    {/*this where our children will be added*/}
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
