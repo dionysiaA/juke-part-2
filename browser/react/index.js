f00a13e00a434372ff30b55081c909a46cb88377
chaimsash <me@chaimsash.com>
2017-03-28 18:09:53 -0400

    redirect working

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, Redirect } from 'react-router';
+import Albums from './components/Albums.js';
+import Album from './components/Album';
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="/" component={AppContainer} >
-    {/*this where our children will be added*/}
+    <Redirect from='/' to='/albums' />
+    <Route path="/" component={AppContainer}>
+      <Route path='/albums' component={Albums} />
     </Route>
   </Router>,
   document.getElementById('app')
